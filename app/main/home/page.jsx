69cff9e13588250e57506c1b4430cc7d8acc10a0
wasim altayyan <wasimtayyan@gmail.com>
2023-06-30 14:07:35 +0300

    create the structure files

---
+++ app/main/home/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div className="m-20">home</div>;
+}
+
+export default page;
---
+++ app/main/layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function layout() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li className="m-20">item</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default layout;
